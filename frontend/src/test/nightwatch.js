efd088a9dfda8e08f39b696843445068d248e45a
solven <solven>
2020-03-12 16:02:03 -0500

    Tester

--- frontend/src/test/nightwatch.js
+++ frontend/src/test/nightwatch.js
@@ -1,12 +1,12 @@
 //Ruta en dond se guarda las imagenes
-var routeImages = './build/tests/images-';
+var routeImages = './build/test/images-';
 
 //Crear Carpeta 
 var mkdirp = require('mkdirp');
 
 
 
-mkdirp('/build/tests/images'+ dateFormat, function (err) {
+mkdirp('/build/test/images'+ dateFormat, function (err) {
     if (err) console.error(err)
     else console.log('pow!')
 });
@@ -35,18 +35,7 @@ module.exports = {
             .maximizeWindow()
             .waitForElementPresent('body', 2000)
             .pause(2000)
-                .waitForElementVisible('body', 1000)
-                .execute(function () {
-                    $(window).scrollTop($('body').offset().top - ($(window).height() / 2));
-                }, [])
                 .saveScreenshot(routeformat)
-            .pause(2000)
-                .waitForElementVisible('.sec-workus', 1000)
-                .execute(function () {
-                    $(window).scrollTop($('.sec-workus').offset().top - ($(window).height() / 2));
-                }, [])
-                .saveScreenshot(routeformat)
-            .pause(2000)
             .end();
     }
 };
